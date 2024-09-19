3ce63c1114ce50284c64f215725a10713bc156cf
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-19 15:02:04 -0600

    Create a new folder inside the components named Sidebar and add new files/tsx inside of it

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+'use client'; // to indicate that this code will run on the client side or browser
+
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index

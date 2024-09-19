b659f9105ee844d175055887b1b0929352ca4c83
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-19 14:55:38 -0600

    Create a component named DefaultLayout for the  sidebar and headers

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -1,1 +1,23 @@
-"use client";
+"use client";  // to indicate that this code will run on the client side or browser
+
+import React, { useState, useLayoutEffect } from 'react';
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='flex'>
+      {/* sidebar */}
+      <div className='relative flex flex-1 flex-col lg:ml-72.5'>
+        {/* headers */}
+        <main>
+          <div className='mx-auto max-w-screen-2xl p-4 dark:bg-[#121212] md:p-6 2xl:p-10'>
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}

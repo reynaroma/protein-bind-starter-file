299138db98169420070cdfc60c924ffbd7e5be5b
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-19 17:05:55 -0600

    Add dynamic sidebar link with dropdown and hover effect

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -29,12 +29,23 @@ const SidebarItem = ({ item, pageName, setPageName }: any) => {
       <li>
         <Link
           href={item.route}
+          onClick={handleClick}
+          className={`${isItemActive ? "rounded-md bg-graydark dark:bg-[#1e1e1e]"
+            : ""
+            } group relative flex items-center gap-2.5 rounded-md px-4 py-2 text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-[#1e1e1e]`}
         >
-
+          {item.icon}
+          {item.label}
         </Link>
+
+        {item.children && (
+          <div className={`translate transform overflow-hidden ${pageName !== item.label.toLowerCase() && "hidden"}`}>
+            <SidebarDropdown item={item.children} />
+          </div>
+        )}
       </li>
     </>
   )
 }
 
-export default SidebarItem
+export default SidebarItem;

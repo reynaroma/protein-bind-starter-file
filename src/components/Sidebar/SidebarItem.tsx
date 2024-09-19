bae9e7068465b5dda9a9e60d4eb34fd2d15bc8bc
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-19 16:09:11 -0600

    Create a component for the SidebarDropdown

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -5,7 +5,12 @@ import { usePathname } from 'next/navigation';
 // components
 import SidebarDropdown from './SidebarDropdown';
 
-const SidebarItem = () => {
+const SidebarItem = ({ item, pageName, setPageName }: any) => {
+
+  const handleClick = () => {
+    const updatePageName = pageName !== item.label.toLowerCase() ? item.
+  }
+
   return (
     <div>SidebarItem</div>
   )

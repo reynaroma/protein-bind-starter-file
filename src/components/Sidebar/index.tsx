7f106cd7e709004df3c35410e17872ddb7becede
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-19 18:18:21 -0600

    Add an array of object for the icons, labels, and route menuItems

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -8,7 +8,40 @@ import { LayoutGrid, Atom, Network, Microscope, Settings, MessageSquareText, Che
 interface SidebarProps {
   sidebarOpen: boolean;
   setSidebarOpen: (arg: boolean) => void;
-}
+};
+
+const menuGroups = [
+  {
+    name: "",
+    menuItems: [
+      {
+        icon: <LayoutGrid size={25} />,
+        label: "Dashboard",
+        route: "/",
+      },
+      {
+        icon: <Atom size={25} />,
+        label: "Molecules Bank",
+        route: "/molecule-bank",
+      },
+      {
+        icon: <Network size={25} />,
+        label: "Model",
+        route: "/model",
+      },
+      {
+        icon: <Microscope size={25} />,
+        label: "Research",
+        route: "/research",
+      },
+      {
+        icon: <MessageSquareText size={25} />,
+        label: "Messages",
+        route: "/message",
+      },
+    ],
+  },
+];
 
 // components
 import SidebarItem from "./SidebarItem";

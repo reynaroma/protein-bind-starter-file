aa62fe1776cdcc8d96fa5450933d964b2f487128
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-19 18:13:41 -0600

    Add an interface to define the types of the object properties

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,6 +5,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { LayoutGrid, Atom, Network, Microscope, Settings, MessageSquareText, ChevronLeftIcon } from 'lucide-react';
 
+interface SidebarProps {
+  sidebarOpen: boolean;
+  setSidebarOpen: (arg: boolean) => void;
+}
+
 // components
 import SidebarItem from "./SidebarItem";
 

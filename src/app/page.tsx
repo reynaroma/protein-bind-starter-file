287e0ef43824880b68a706df634310dcea9758ff
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-24 18:00:46 -0600

    Add a metadata object and use the Metadata interface to define the type and add title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 import { Metadata } from "next";
 
-
+export const metadata: Metadata ={
+  title: "Protein Bind: a leading research platform for drug discovery.",
+  description: "This is a description for protein bind",
+}
 
 export default function Home() {
   return (

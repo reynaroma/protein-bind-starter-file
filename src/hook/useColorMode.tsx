bd3ac846a6f2370b117cbe9dce501475c0c95583
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-26 15:55:46 -0600

    Create a new hook useColorMode and import the useLocalStorage hook and useEffect

---
+++ src/hook/useColorMode.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useColorMode = () => {
+  return (
+    <div>useColorMode</div>
+  )
+}
+
+export default useColorMode

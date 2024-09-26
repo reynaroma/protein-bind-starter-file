5aed9ca07a06814e152b1de2b5417632b065be9d
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-26 15:30:22 -0600

    Create a dropdown menu with the My profile and settings link

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -39,6 +39,31 @@ const DropdownUser = () => {
         </span>
         <ChevronDown />
       </Link>
+
+      {dropdownOpen && (
+        <div className="absolute right-0 mt-4 flex w-62.5 flex-col rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+          <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-7.5 dark:border-strokedark">
+            <li>
+              <Link
+                href="/profile"
+                className="flex items-center gap-3.5 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
+              >
+                <User2 />
+                My Profile
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/settings"
+                className="flex items-center gap-3.5 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
+              >
+                <Settings />
+                Account Settings
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }

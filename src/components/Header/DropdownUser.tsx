a5c5798cc6de3ec2306346094bee84aba317a346
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-26 15:40:15 -0600

    Add an onClick property on the button

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -64,6 +64,7 @@ const DropdownUser = () => {
           </ul>
           <button
             type="button"
+            // onClick={handleLogout}
             className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
           >
             <LogOut />

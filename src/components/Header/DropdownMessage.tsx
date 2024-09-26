678d1f2fe3d27669df7290d8861ecacf8eb55dd6
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-26 13:09:43 -0600

    Update DropdownMessage.tsx
    Add useState for the dropdown and notification

--- src/components/Header/DropdownMessage.tsx
+++ src/components/Header/DropdownMessage.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { MessageCircleMore } from "lucide-react";
 
 const DropdownMessage = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [notifying, setNotifying] = useState(true);
+  
   return (
     <li className="relative">DropdownMessage</li>
   )

70248e04b322eafe2c4ed64ac9bb989fcfc770f1
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-26 13:06:10 -0600

    Update DropdownMessage.tsx
    Add a new DropdownMessage component to the application

--- src/components/Header/DropdownMessage.tsx
+++ src/components/Header/DropdownMessage.tsx
@@ -2,3 +2,10 @@ import { useState } from "react";
 import Link from "next/link";
 import { MessageCircleMore } from "lucide-react";
 
+const DropdownMessage = () => {
+  return (
+    <li className="relative">DropdownMessage</li>
+  )
+}
+
+export default DropdownMessage

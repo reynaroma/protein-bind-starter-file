9ebd371b281efacf7435d921d8c79203646d015f
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-26 15:33:57 -0600

    Add a logout button on the dropdown menu

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -62,6 +62,13 @@ const DropdownUser = () => {
               </Link>
             </li>
           </ul>
+          <button
+            type="button"
+            className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
+          >
+            <LogOut />
+            Log Out
+          </button>
         </div>
       )}
     </div>

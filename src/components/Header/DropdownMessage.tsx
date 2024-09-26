653829ed21fb7b208e09d3b3fd0520ed648053ca
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-26 13:26:29 -0600

    Add a message indicator to the message link

--- src/components/Header/DropdownMessage.tsx
+++ src/components/Header/DropdownMessage.tsx
@@ -5,9 +5,18 @@ import { MessageCircleMore } from "lucide-react";
 const DropdownMessage = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [notifying, setNotifying] = useState(true);
-  
+
   return (
-    <li className="relative">DropdownMessage</li>
+    <li className="relative">
+      <Link
+        href="/message"
+        className="relative flex h-8.5 w-8.5 items-center justify-center rounded-full border-[0.5px] border-stroke bg-gray hover:text-primary dark:border-strokedark dark:bg-meta-4 dark:text-white"
+      >
+        <span className={`absolute -right-0.5 -top-0.5 z-1 h-2 w-2 rounded-full bg-meta-1 ${notifying === false ? "hidden" : "inline"}`}>
+          <span></span>
+        </span>
+      </Link>
+    </li>
   )
 }
 

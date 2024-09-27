8795e04b9ff6ab74e8ae39ba38c5b33493313c7f
Reyna May Roma <roma.reynamay@gmail.com>
2024-09-26 21:11:42 -0600

    Import the components needed in the Header and create the component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import DropdownMessage from "./DropdownMessage";
+import { SearchIcon } from "lucide-react";
+import DropdownUser from "./DropdownUser";
+
+const Header = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Header;

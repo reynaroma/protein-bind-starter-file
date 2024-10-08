31737457cf3bb15e3ea486e21cf4d8e9baceff87
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-08 15:27:38 -0600

    Create a structure for the button and import the Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,10 +4,33 @@ import DropdownMessage from "./DropdownMessage";
 import { SearchIcon } from "lucide-react";
 import DropdownUser from "./DropdownUser";
 import Image from "next/image";
+import React from "react";
 
-const Header = () => {
+const Header = (props: {
+  sidebarOpen: string | boolean | undefined;
+  setSidebarOpen: (arg: boolean) => void;
+}) => {
   return (
-    <div>index</div>
+    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-[#121212] dark:drop-shadow-xl">
+      <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
+        <button
+          aria-controls="sidebar"
+          onClick={(e) => {
+            e.stopPropagation();
+            props.setSidebarOpen(!props.sidebarOpen);
+          }}
+          className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+        >
+          <span>
+            <span>
+              <span>
+
+              </span>
+            </span>
+          </span>
+        </button>
+      </div>
+    </header>
   )
 }
 
--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -4,6 +4,7 @@ import React, { useState, useLayoutEffect } from 'react';
 
 // components
 import Sidebar from '../Sidebar';
+import Header from '../Header';
 
 export default function DefaultLayout({
   children,
@@ -18,6 +19,7 @@ export default function DefaultLayout({
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className='relative flex flex-1 flex-col lg:ml-72.5'>
         {/* headers */}
+        <Header />
         <main>
           <div className='mx-auto max-w-screen-2xl p-4 dark:bg-[#121212] md:p-6 2xl:p-10'>
             {children}

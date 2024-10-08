7a6b24ac782ba329ecb3829d049aaceee4e7b71b
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-08 14:36:29 -0600

    Add the components needed for the index of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
+import DarkModeSwitcher from "./DarkModeSwitcher";
 import DropdownMessage from "./DropdownMessage";
 import { SearchIcon } from "lucide-react";
 import DropdownUser from "./DropdownUser";
+import Image from "next/image";
 
 const Header = () => {
   return (

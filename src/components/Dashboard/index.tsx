993096dd64201cf3ad189034fb90177692bb9adc
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-09 17:10:28 -0600

    Build the index component for the dashboard, pass down all the title, subtile and icon as a child then render the DashboardCardChat and map components, and style it.

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -20,9 +20,29 @@ const DashboardCardChat = dynamic(
   },
 );
 
-const Index = () => {
+const Index: React.FC = () => {
   return (
-    <div>Index</div>
+    <>
+      <div>
+        <CTACard subtitle="get access to more molecules" title="Molecule Bank">
+          <AtomIcon />
+        </CTACard>
+        <CTACard subtitle="get access to more molecules" title="Generate Molecules">
+          <Network />
+        </CTACard>
+        <CTACard subtitle="get access to more molecules" title="Search Compounds">
+          <SearchIcon />
+        </CTACard>
+        <CTACard subtitle="get access to more molecules" title="Collaborative Research">
+          <MessageCircle />
+        </CTACard>
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <DashboardCardChat />
+        <DashboardCardMap />
+      </div>
+    </>
   )
 }
 

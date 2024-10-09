4dc386fb219f299a0eb2e005a6c21c3543a4ea92
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-09 15:23:36 -0600

    Create the structure of what the DashboardCardChat component gonna look like

--- src/components/Dashboard/Components/DashboardCardChat.tsx
+++ src/components/Dashboard/Components/DashboardCardChat.tsx
@@ -1,9 +1,39 @@
+"use client";
 import React from 'react';
+import Image from 'next/image';
 
-const DashboardCardChat = () => {
+const ChartThree: React.FC = () => {
   return (
-    <div>DashboardCardChat</div>
+    <div>
+      <div>
+        <div>
+          <h5>Collaborate with team</h5>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            <img src="/images/user/user-01.png" alt="Jese image" />
+            <div>
+              <div>
+                <span>
+                  {"John Doe"}
+                </span>
+                <span>
+                  11:46
+                </span>
+              </div>
+              <p>That's awesome. I think we are making a pretty good progress.</p>
+              <span>
+                Delivered
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default DashboardCardChat
+export default ChartThree;

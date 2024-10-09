12d8e967aab14c247d0f1b9600276a64556cc9fa
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-09 14:20:37 -0600

    Implement the type of the CTACard component as React.FC<CTACardProps>

--- src/components/Dashboard/Components/CTACard.tsx
+++ src/components/Dashboard/Components/CTACard.tsx
@@ -7,7 +7,7 @@ interface CTACardProps {
   children: ReactNode;
 }
 
-const CTACard = () => {
+const CTACard: React.FC<CTACardProps> = ({ title, subtitle, children }) => {
   return (
     <div>CTACard</div>
   )

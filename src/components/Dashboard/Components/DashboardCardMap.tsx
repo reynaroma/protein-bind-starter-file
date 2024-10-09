09d4720915f9104a1262793dcc6f9c768d88d5c4
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-09 16:46:49 -0600

    Import the the code needed and use useEffect hook to run the World Map

--- src/components/Dashboard/Components/DashboardCardMap.tsx
+++ src/components/Dashboard/Components/DashboardCardMap.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from 'react';
+import jsVectorMap from 'jsvectormap';
+import "jsvectormap/dist/jsvectormap.css";
+import "../../../js/us-aea-en"
+import "../../../js/world";
+const MapOne: React.FC = () => {
 
-const DashboardCardMap = () => {
+  useEffect(() => {
+    const mapOne = new jsVectorMap({
+      selector: "#mapOne",
+      map: "world",
+      zoomButtons: true,
+      
+      regionStyle: {
+        initial: {
+          fill: "#C8D0D8",
+        },
+        hover: {
+          fillOpacity: 1,
+          fill: "#3056D3",
+        },
+      },
+      regionLableStyle: {
+        initial: {
+          fontFamily: "Satoshi",
+          fontWeight: "semibold",
+          fill: "#fff",
+        },
+        hover: {
+          cursor: "pointer",
+        },
+      },
+
+      labels: {
+        regions: {
+          render(code: string) {
+            return code.split("-")[1];
+          },
+        },
+      },
+    });
+
+    return () => {
+      const map = document.getElementById("mapOne");
+      if (map) {
+        map.innerHTML = "";
+      }
+    };
+  }, [])
+
   return (
-    <div>DashboardCardMap</div>
+    <div>MapOne</div>
   )
 }
 
-export default DashboardCardMap
+export default MapOne

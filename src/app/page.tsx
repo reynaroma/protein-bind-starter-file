eed72d696d1ae39c25e7c025e85d2834d6b34d80
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-09 17:11:53 -0600

    Import the index component to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
-
 import { Metadata } from "next";
 
-export const metadata: Metadata ={
+// components
+import Index from "@/components/Dashboard";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
   title: "Protein Bind: a leading research platform for drug discovery.",
   description: "This is a description for protein bind",
 }
@@ -11,7 +13,7 @@ export default function Home() {
   return (
     <>
       <DefaultLayout>
-        <p>Hello dashboard page</p>
+        <Index />
       </DefaultLayout>
     </>
   );

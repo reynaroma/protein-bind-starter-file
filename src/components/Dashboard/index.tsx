bc26af28e91103749d0a9a603bf57550a4ee5d94
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-09 16:54:54 -0600

    Import the dynamic from next, icons from lucide-react and the CTACard component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+"use client";
+import dynamic from "next/dynamic";
+import React from 'react';
+import { AtomIcon, MessageCircle, Network, SearchIcon } from "lucide-react";
+
+// components
+import CTACard from "./Components/CTACard";
 
 const Index = () => {
   return (

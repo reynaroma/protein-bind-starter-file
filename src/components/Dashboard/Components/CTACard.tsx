69856081007f9202b45ccbccf535a011c4e7dfb7
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-09 14:14:25 -0600

    Define CTACardProps interface with title, subtitle and children

---
+++ src/components/Dashboard/Components/CTACard.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+import { ArrowRight } from 'lucide-react';
+
+interface CTACardProps {
+  title: string;
+  subtitle: string;
+  children: ReactNode;
+}
+
+const CTACard = () => {
+  return (
+    <div>CTACard</div>
+  )
+}
+
+export default CTACard
